56d83ee8e68f52efa13a0cb4d2019fb2b035e643
TomLaclavere <laclavere@apc.in2p3.fr>
2024-12-06 11:05:28 +0100

    Remove sphinx_autodoc_typehint

--- sphinx_documentation/Documentation_test/docs/source/conf.py
+++ sphinx_documentation/Documentation_test/docs/source/conf.py
@@ -25,7 +25,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    'sphinx_autodoc_typehints',
     "numpydoc",
     'sphinx.ext.mathjax',
 ]
