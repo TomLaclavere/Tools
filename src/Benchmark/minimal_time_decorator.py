a7f63e49207046cd4838d6784ead118cea29dbd1
TomLaclavere <129369785+TomLaclavere@users.noreply.github.com>
2025-12-04 11:43:12 +0100

    Implement timing decorator for sum functions
    Added numpy and Python sum functions with timing decorator.

--- src/Benchmark/minimal_time_decorator.py
+++ src/Benchmark/minimal_time_decorator.py
@@ -2,6 +2,9 @@
 import time
 from functools import wraps
 
+import numpy as np
+
+
 def measure_time(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -13,3 +16,24 @@ def measure_time(func):
         return result, elapsed_time
 
     return wrapper
+
+
+@measure_time
+def numpy_sum(x):
+    return np.sum(x)
+
+
+@measure_time
+def python_sum(x):
+    sum = 0
+    for i in range(len(x)):
+        sum += x[i]
+    return sum
+
+
+# Test
+np_x = np.ones(10000000)
+list_x = list(np_x.copy())
+
+numpy_sum(np_x)
+python_sum(list_x)
